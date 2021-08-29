67b3201c17a276628dfd901979031787740843c3
MingXin <smallstars10@gmail.com>
2021-08-29 17:12:08 +0800

    新增輪播牆

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-$("#goDownBtn").click(function () {});
+$(".slider div").eq(0).css("background", "#fff"); // 按鈕白色初始化
+let circles = document.querySelectorAll(".circle");
+let slides = document.querySelectorAll(".banner");
+let index = 0;
+let myIntervalID = "";
+
+function prev() {
+  clearInterval(myIntervalID);
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+  $(".slider div")
+    .eq(index)
+    .css("background", "#fff")
+    .siblings()
+    .css("background", "transparent");
+
+  startInterval();
+}
+
+function next() {
+  clearInterval(myIntervalID);
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+  $(".slider div")
+    .eq(index)
+    .css("background", "#fff")
+    .siblings()
+    .css("background", "transparent");
+  startInterval();
+}
+
+function startInterval() {
+  myIntervalID = setInterval(() => {
+    if (index > 2) {
+      index = 0;
+    }
+    next();
+    $(".slider div")
+      .eq(index)
+      .css("background", "#fff")
+      .siblings()
+      .css("background", "transparent");
+  }, 4000);
+}
+startInterval();
+
+for (let i = 0; i < circles.length; i++) {
+  circles[i].addEventListener("click", function () {
+    clearInterval(myIntervalID);
+    slides[index].classList.remove("active");
+    index = $(this).index();
+    slides[index].classList.add("active");
+    $(this)
+      .css("background", "#fff")
+      .siblings()
+      .css("background", "transparent");
+    startInterval();
+  });
+}
