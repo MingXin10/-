c34dce258d597fa469e5d387ab55e8fdf1baeb79
MingXin <smallstars10@gmail.com>
2021-09-01 23:12:23 +0800

    新增navbar載入特效，部分skrollr特效

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let s = skrollr.init();
+
 $(".slider div").eq(0).css("background", "#fff"); // 按鈕白色初始化
 let circles = document.querySelectorAll(".circle");
 let slides = document.querySelectorAll(".banner");
@@ -70,5 +72,30 @@ $(window).scroll((e) => {
     $("#navbar").removeClass("navWhite");
     $("li a").removeClass("black");
     $("#nav-fb").removeClass("black");
+  }
+});
+
+$(document).ready(function () {
+  $("nav").removeClass("hide");
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 0) {
+    $("#logo").removeClass("logo-big");
+    $("#logo").addClass("logo-small");
+  } else {
+    $("#logo").removeClass("logo-small");
+    $("#logo").addClass("logo-big");
+  }
+});
+
+$(window).scroll(function () {
+  console.log($(window).scrollTop());
+  if ($(window).scrollTop() > 850) {
+    $(".p1Text").css("opacity", 1).css("bottom", 0);
+    $(".p2Text").css("opacity", 1).css("top", 0);
+  } else {
+    $(".p1Text").css("opacity", 0).css("bottom", "75px");
+    $(".p2Text").css("opacity", 0).css("top", "75px");
   }
 });
